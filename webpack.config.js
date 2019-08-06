67e8e37c826efdaa38ad0f860eedee57e45c6448
Kaho Cheung <kaho@elefanttraks.com>
2019-08-06 23:57:09 +1000

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
+  },
+};
