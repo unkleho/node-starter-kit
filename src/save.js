df916da5a4729e6d2afff370a58b08b9e32ccd86
Kaho Cheung <kaho@elefanttraks.com>
2019-08-09 14:24:26 +1000

    Add dai-ichi data

--- src/save.js
+++ src/save.js
@@ -2,10 +2,16 @@ import fs from 'fs';
 
 import scrape from './scraper';
 
-const save = async id => {
+const save = async (id) => {
   const file = await scrape(id);
 
   fs.writeFileSync('./test.json', JSON.stringify(file));
 };
 
-save('n7oVw8Zn');
+// May Gibbs
+// n7oVw8Zn
+
+// Tal Dai-ichi
+// YK5QWJjn
+
+save('YK5QWJjn');
